ce5db1d55312c1e7ece68912088f91e166aff017
aszczur <39106960+aszczur@users.noreply.github.com>
2018-08-07 20:55:56 -0400

    Update startingLocation.jl

--- src/startingLocation.jl
+++ src/startingLocation.jl
@@ -9,15 +9,15 @@ mutable struct DA_id_coord
     coordinates::Tuple{Float64, Float64}
 end
 
+"""
+Selects starting DA_home for an agent randomly weighted by weight_var
+    
+**Arguments**
+* `dict_df_DAcentroids` : dictionary of dataframes with :LATITUDE and :LONGITUDE for each DA
+* `df_demostat_weight_var` : dataframe with weight_var value for each DA
+* `weight_var` : weighting variable name
+"""
 function startLocationSelector(dict_df_DAcentroids, df_demostat_weight_var, weight_var)::DA_id_coord
-    
-    # Selects starting DA_home for an agent randomly weighted by weight_var
-    
-    # Args:
-    # - dict_df_DAcentroids - dictionary of dataframes with :LATITUDE and :LONGITUDE for each DA
-    # - df_demostat_weight_var - dataframe with weight_var value for each DA
-    # - weight_var - weighting variable name
-    
     
     DA_home = sample(df_demostat_weight_var[:PRCDDA], fweights(df_demostat_weight_var[weight_var]))
     point_DA_home = dict_df_DAcentroids[DA_home][1, :LATITUDE], 
