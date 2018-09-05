7d54031faec453832c6f80bd16bbd4eb8b63d6b0
Przemyslaw Szufel <pszufe@gmail.com>
2018-09-05 22:06:43 +0200

    results

--- src/simulation.jl
+++ src/simulation.jl
@@ -12,3 +12,9 @@ mode = "business";
 
 sim_data = get_sim_data(datapath);
 nodes, buffer = run_simulation(sim_data,mode,N);
+
+nodeids = collect(keys(nodes));
+sort!(nodeids, by = (nodeid -> nodes[nodeid].count), rev=true);
+for i in nodeids[1:5]
+    show(nodes[i]); println();
+end
