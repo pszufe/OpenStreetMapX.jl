0dce81d55ffcd3fe4806956ec31bd0bfc09247e3
Przemyslaw Szufel <pszufe@gmail.com>
2018-09-10 16:26:57 +0000

    resolving CSV issues

--- src/simulation.jl
+++ src/simulation.jl
@@ -12,26 +12,32 @@ mode = "business";
 
 sim_data = get_sim_data(datapath);
 
-
-
 using Distributed
+using Random
+using Dates
 #Distributed.addprocs(4)
 
-@Distributed.distributed for i in range(1000)
+@Distributed.distributed for i in 1:1
     Random.seed!(i);
+    startt=Dates.now()
     nodes, buffer = run_simulation(sim_data,mode,N);
-
+    @info "Average time per simulation $((Dates.now()-startt).value/N)ms"
+    
     nodeids = collect(keys(nodes));
     sort!(nodeids, by = (nodeid -> nodes[nodeid].count), rev=true);
-    #for i in nodeids[1:5]
-    #    show(nodes[i]); println();
-    #end
-    #- scalic nodes w jeden duzy dataframe, ktory ma kolumne dodatkowe kolumny: nodeid oraz  Distributed.myid()
-    #- zapisac na dysk do katalogu plik = "/home/ubuntu/results/$(myid())-results-000i"
-    # - `aws s3 --region ca-central-1 cp  $plik "s3://eag-ca-bucket-1/SimResults/`
-
+    for i in nodeids[1:1]
+        show(nodes[i]); println();
+    end
 end
+
 #TODO
-# wykonac powyzsze polecenie powloki (sprawdzic jak)
-# napisac do Tonego o zwiekszenie limitu serwery.
-# wykresy dla wynikow - w R
+    #- scalic nodes w jeden duzy dataframe, ktory ma kolumne dodatkowe kolumny: nodeid oraz  Distributed.myid()
+    #- zapisac na dysk do katalogu plik = "/home/ubuntu/results/$(myid())-results-000i"-    # - `aws s3 region ca-central-1 cp  $plik "s3://eag-ca-bucket-1/SimResults/`
+-# wykonac powyzsze polecenie powloki (sprawdzic jak)
+-# napisac do Tonego o zwiekszenie limitu serwery.
+-# wykresy dla wynikow - w R
+
+
+
+
+
