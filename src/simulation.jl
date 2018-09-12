4ca132c9b3c1720fa07678f9ed0eda8ecc3b6782
Przemyslaw Szufel <pszufe@gmail.com>
2018-09-12 11:38:22 +0000

    simulation parallelization

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,34 +1,43 @@
-pth = "osm/";
-path = "sim/";
-datapath = "../../datasets/";
 
-include(pth*"OpenStreetMap.jl")
-include(path*"OSMSim.jl")
+using Distributed
+Distributed.addprocs(2)
 
-using Main.OSMSim
+@everywhere begin
+    using Random
+    using Dates
+    pth = "osm/";
+    path = "sim/";
+    datapath = "../../datasets/";
+    include(joinpath(pth,"OpenStreetMap.jl"))
+    include(joinpath(path,"OSMSim.jl"))
+    using Main.OSMSim
+    mode = "business";
+end
 
 N = 100;
-mode = "business";
 
 sim_data = get_sim_data(datapath);
 
-using Distributed
-using Random
-using Dates
-#Distributed.addprocs(4)
-
-@Distributed.distributed for i in 1:1
+@info "Starting on $(nworkers()) workers"
+#i = 1
+simcount = @Distributed.distributed (+) for i in 1:4
+    println("Worker $(myid()) Starting simulation for seed $i")
     Random.seed!(i);
-    startt=Dates.now()
+    
+    startt = Dates.now()
     nodes, buffer = run_simulation(sim_data,mode,N);
-    @info "Average time per simulation $((Dates.now()-startt).value/N)ms"
+    println("Average time per simulation $((Dates.now()-startt).value/N)ms")
     
     nodeids = collect(keys(nodes));
     sort!(nodeids, by = (nodeid -> nodes[nodeid].count), rev=true);
     for i in nodeids[1:1]
         show(nodes[i]); println();
     end
+    1
 end
+
+@info "completed all $simcount simulation group  runs"
+
 
 #TODO
     #- scalic nodes w jeden duzy dataframe, ktory ma kolumne dodatkowe kolumny: nodeid oraz  Distributed.myid()
