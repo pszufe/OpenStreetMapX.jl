a01157af79248d1f8309ef0bc7a88fc27bbd7297
bartoszpankratz <32309411+bartoszpankratz@users.noreply.github.com>
2018-09-22 20:44:29 -0400

    changes

--- src/compare_data.jl
+++ src/compare_data.jl
@@ -11,31 +11,31 @@ include(joinpath(path,"OSMsim.jl"))
 
 using Main.OSMSim
 using DataFrames
-using CSVFiles
+using Nanocsv
 using GLM
 
 function compare_data(datapath::String,mapfile::String,resultfile::String, testfile::String)
     bounds,nodes,roadways,intersections,network = OSMSim.read_map_file(datapath,mapfile)
-    frame = DataFrames.DataFrame(Node_ID = Int[],empirical = Int[],simulation = Int[])
-    traffic = DataFrames.DataFrame(CSVFiles.load(joinpath(datapath,testfile)))
-    traffic_data = Dict{Int,Int}()
+    frame = DataFrames.DataFrame(Node_ID = Int[],latitude = Float64[], longitude = Float64[], empirical = Int[],simulation = Int[])
+    traffic = Nanocsv.read_csv(joinpath(datapath,testfile))
+    traffic_data = Dict()
     for i in 1:nrow(traffic)
         key = OpenStreetMap.nearest_node(nodes,OpenStreetMap.ENU(OpenStreetMap.LLA(traffic[:LATITUDE][i],traffic[:LONGITUDE][i]),bounds),collect(keys(intersections)))
         if haskey(traffic_data, key)
-            traffic_data[key] += traffic[i,:TRAFFIC1]
+            traffic_data[key][:TRAFFIC1][1] += traffic[i,:TRAFFIC1]
         else
-            traffic_data[key] = traffic[i,:TRAFFIC1]
+            traffic_data[key] = traffic[i,[:LATITUDE,:LONGITUDE,:TRAFFIC1]]
         end
     end
     lines = readlines(open(joinpath(datapath,resultfile)))
     for line in lines
         x = parse.(Int,split(line))
         if haskey(traffic_data, x[1])
-            push!(frame,(x[1],traffic_data[x[1]],x[2]))
+            push!(frame,(x[1],traffic_data[x[1]][:LATITUDE][1], traffic_data[x[1]][:LONGITUDE][1],traffic_data[x[1]][:TRAFFIC1][1], x[2]))
         end
     end
     model = GLM.lm(@formula(simulation ~ empirical - 1),frame)
-    frame[:scaled_emprical] = GLM.predict(model)
+    frame[:scaled_empirical] = GLM.predict(model)
     return frame, model
 end
 
