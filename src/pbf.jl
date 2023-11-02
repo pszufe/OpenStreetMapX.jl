58150fd957a39f03cfb42a8ceeb0fdac73793cee
Benoît Legat <benoit.legat@gmail.com>
2023-11-02 11:41:28 +0100

    [PBF] Fix conversion from enum to its name

--- src/pbf.jl
+++ src/pbf.jl
@@ -107,7 +107,7 @@ function process_element(osm, pbf_relation::OSMPBF.Relation, table, lat_offset, 
     types = pbf_relation.types
     for i in eachindex(types)
         push!(relation.members, Dict(
-            "type" => OSMPBF.var"Relation.MemberType"[types[i] + 1],
+            "type" => string(types[i]),
             "ref" => string(memids[i]),
         ))
     end
